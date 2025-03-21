3bd0579218d13224b7052f880612472d6e609b68
JuukoFrancis <juukojuniorfrancis@gmail.com>
2025-03-22 01:10:34 +0300

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -142,6 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:5174",
+    "https://cpu-frontend.onrender.com",
     "http://127.0.0.1:5173",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
